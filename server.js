7905c2c3c51ab57f1ce2c3f9bdf12a43df3db3ea
Jason Garcia <jaygarcia0530@gmail.com>
2023-06-04 19:31:39 -0700

    set up routes

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.get('/api/v1/restaurants', async (req, res) => {
 
     try {
         const results = await db.query("SELECT * FROM restaurants");
-        console.log(results);
         res.status(200).json({
             status: "success",
             results: results.rows.length,
@@ -30,7 +29,7 @@ app.get('/api/v1/restaurants', async (req, res) => {
 app.get('/api/v1/restaurants/:id', async (req, res) => {
     try {
         const id = req.params.id;
-        const results = await db.query("SELECT * FROM restaurants WHERE id = $1", [id]);
+        const results = await db.query("SELECT $2 FROM restaurants WHERE id = $1", [id, "name"]);
         res.status(200).json({
             status: "success",
             data: {
@@ -44,21 +43,56 @@ app.get('/api/v1/restaurants/:id', async (req, res) => {
 });
 
 // Create restaurant
-app.post('/api/v1/restaurants/', (req, res) => {
+app.post('/api/v1/restaurants/', async (req, res) => {
     console.log(req.body);
-    res.status(201).json("restaurant created");
+
+    const { name, location, price_range } = req.body;
+    try {
+        const results = await db.query("INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) returning *", [name, location, price_range]);
+        console.log(results);
+        res.status(201).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0]
+            }
+        });
+
+    } catch (e) {
+
+    }
 });
 
 // Update restaurant
-app.put('/api/v1/restaurants/:id', (req, res) => {
-    console.log(req.body);
-    res.status(200).json("restaurant updated");
+app.put('/api/v1/restaurants/:id', async (req, res) => {
+
+    try {
+        const { name, location, price_range } = req.body;
+        const id = req.params.id;
+        const results = await db.query("UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 returning *", [name, location, price_range, id]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurants: results.rows[0]
+            }
+        });
+    } catch (e) {
+
+    }
+
+
 });
 
 // Delete restaurant
-app.delete('/api/v1/restaurants/:id', (req, res) => {
-    console.log(req.params)
-    res.status(200).json("restaurant deleted");
+app.delete('/api/v1/restaurants/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const results = await db.query("DELETE restaurants WHERE id = $1", [id]);
+        res.status(204).json({
+            status: "success"
+        });
+    } catch (e) {
+
+    }
 });
 
 app.listen(PORT, () => {
